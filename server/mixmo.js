42bd12c2dac02001e5f365070fa8ec42c3fc919d
Giorgiolino <gnjock14@gmail.com>
2014-12-08 02:07:53 +0100

    (WIP) letters dispatch on game start

--- server/mixmo.js
+++ server/mixmo.js
@@ -72,19 +72,49 @@ Meteor.startup(function () {
             alphabet = createGameLetters();
         },
         createRoom: function(name){
+            console.info("leaveRoom | name : "+name+", user : " + Meteor.userId());
             Room.createNewRoom(Meteor.userId(), name);
         },
         joinRoom: function(idRoom){
+            console.info("joinRoom | room "+idRoom+", user : " + Meteor.userId());
             Room.joinRoom(Meteor.userId(), idRoom);
         },
         leaveRoom: function(idRoom){
+            console.info("leaveRoom | room "+idRoom+", user : " + Meteor.userId());
             Room.leaveRoom(Meteor.userId(), idRoom);
         },
         startGame: function(idRoom){
-            Rooms.update(idRoom, {$set: {status: ROOM_CLOSED}});
+            console.info("startGame | room "+idRoom);
+            // Close room --> status "closed"
+            // Init game letters in room
+            // Give each player 6 letters
+            var chars_per_player = 6; // Number of letters for this time
+            var mRoom = Rooms.findOne(idRoom);
+            var mPlayers = mRoom.players;
+            var newPlayers = [];
+            var roomLetters = createGameLetters();
+            for(var i=0,len=mPlayers.length; i<len; i++){
+              console.log(" For loop i --> " +i);              
+              var playerLetters = [];
+              for(var j=0;i<chars_per_player;j++){
+                playerLetters.push({value : roomLetters.pop()});
+              }
+              console.log(playerLetters);
+              var player = mPlayers[i];
+              console.log(player);
+              player.myletters = playerLetters;
+              newPlayers.push(player);
+            }
+            Rooms.update(idRoom, {
+              $set: {
+                "status"  : ROOM_CLOSED,
+                "letters" : roomLetters,
+                "players" : newPlayers
+              }
+            });
         },
         playerReady: function(idRoom){
-            console.log("player ready / server");
+            console.info("playerReady | room "+idRoom);
             Room.playerReady(idRoom, Meteor.userId());
         }
     });
